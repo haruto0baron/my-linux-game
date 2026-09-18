c2c21ad327e5b7d20948e426dc28f59e2298082b
haruto0baron <haru_to0baron@outlook.jp>
2026-09-18 18:45:45 +0900

    ウインドウのリサイズに対応

--- scr/main.c
+++ scr/main.c
@@ -1,6 +1,9 @@
 #include "raylib.h"
 
 int main(void) {
+    // リサイズ可
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    
     InitWindow(800, 600, "My Linux Game");
     SetTargetFPS(60);
 
@@ -9,10 +12,24 @@ int main(void) {
 
     while (!WindowShouldClose()) {
         // 更新
+
+        // ウインドウ
+
+        int width = GetScreenWidth();
+        int height = GetScreenHeight();
+
+        //  キー
+
         if (IsKeyDown(KEY_RIGHT)) ball.x += 4.0f;
         if (IsKeyDown(KEY_LEFT))  ball.x -= 4.0f;
         if (IsKeyDown(KEY_UP))    ball.y -= 4.0f;
         if (IsKeyDown(KEY_DOWN))  ball.y += 4.0f;
+
+        // もしはみ出てるなら修正
+        if (ball.x < 0) ball.x = 0;
+        if (ball.x > width) ball.x = width;
+        if (ball.y < 0) ball.y = 0;
+        if (ball.y > height) ball.y = height;
 
         // 描画
         BeginDrawing();
